880268546f038c1920c0dfcdce7e9bc8742d7821
Greg <gcottere@asu.edu>
2018-11-21 10:58:10 -0500

    created dashboard and routing

--- Angular Workspace/budget-site/src/app/app-routing.module.ts
+++ Angular Workspace/budget-site/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+import { InvestmentComponent } from './components/investment/investment.component';
+import { RegisterComponent } from './components/register/register.component';
+import { GoalComponent } from './components/goal/goal.component';
+
+const routes: Routes = [{
+  component: LoginComponent,
+  path: ''
+}, {
+  component: RegisterComponent,
+  path: 'register'
+}, {
+  component: DashboardComponent,
+  path: 'dashboard'
+}, {
+  component: GoalComponent,
+  path: 'goals'
+}, {
+  component: InvestmentComponent,
+  path: 'invest'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
